3b3c014516bb767df5b8d55548ee25eb0267f382
stafford <stafford.williams@gmail.com>
2016-05-25 15:39:43 +1000

    implemented traditional bunnymark implemented scaling

--- core/src/in/atqu/bunnymark/BunnyMark.java
+++ core/src/in/atqu/bunnymark/BunnyMark.java
@@ -3,21 +3,25 @@ package in.atqu.bunnymark;
 import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.input.GestureDetector;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.RandomXS128;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+/*
+  Originally from https://github.com/johngirvin/bunnymark-libgdx/blob/master/core/src/com/nivrig/BunnyMark.java
+ */
 
 public class BunnyMark extends ApplicationAdapter {
 	private SpriteBatch batch;
-
     private Sprite sprite;
-
+    private OrthographicCamera camera;
+    private ScreenViewport viewport;
     private static RandomXS128 random = new RandomXS128();
     private static float minX = 0;
     private static float maxX = 0;
@@ -29,138 +33,59 @@ public class BunnyMark extends ApplicationAdapter {
     private float labelY;
     private String bunnyLabel;
     private String fpsLabel = "";
+    private String sizeLabel = "";
 
-    private class Bunny {
-        float x, y, xv, yv, r, rv;
+    public class Bunny {
+    	float x, y, speedX, speedY;
+    	public Bunny() {
+    		y = maxY/2;
+    		x = 10;
+    		speedY = random.nextInt(500) + 250;
+        	speedX = random.nextInt(500) + 250 - 500;
+    	}
     }
     private Array<Bunny> bunnies = new Array<Bunny>();
 
-	@Override
+    @Override
 	public void create() {
-		batch = new SpriteBatch(5400);
 
+        batch = new SpriteBatch(5400);
         labelFont = new BitmapFont();
         labelFont.setColor(Color.WHITE);
-
         sprite = new Sprite(new Texture("wabbit_alpha.png"));
+        camera = new OrthographicCamera();
+        viewport = new ScreenViewport(camera);
+        viewport.apply();
 
         resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-        moreBunnies(1000);
-
-        switch (Gdx.app.getType()) {
-            case iOS:
-            case Android:
-                Gdx.input.setInputProcessor(new GestureDetector(new GestureDetector.GestureAdapter() {
-                    @Override
-                    public boolean fling(float velocityX, float velocityY, int button) {
-                        if (velocityX < 0) {
-                            lessBunnies();
-                        } else {
-                            moreBunnies(100);
-                        }
-                        return true;
-                    }
-
-                    @Override
-                    public boolean tap(float screenX, float screenY, int count, int button) {
-                        moreBunnies(1000);
-                        return true;
-                    }
-                }));
-                break;
-
-            default:
-                Gdx.input.setInputProcessor(new InputAdapter() {
-                	@Override
-                	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-                		moreBunnies(1000);
-						return false;
-                	}
-                	
-                    @Override
-                    public boolean keyUp(int keycode) {
-                        switch (keycode) {
-                            case Input.Keys.Q:
-                                lessBunnies();
-                                return true;
-                            case Input.Keys.W:
-                                moreBunnies(1000);
-                                return true;
-                            case Input.Keys.S:
-                                moreBunnies(10000);
-                                return true;
-                        }
-                        return false;
-                    }
-                });
-                break;
-        }
+        bunnies.add(new Bunny());
+        bunnies.add(new Bunny());
+        bunnyLabel = "2";
     }
 
     @Override
-    public void resize(int w, int h) {
-        minX = 0;
-        maxX = w - sprite.getWidth();
-        minY = 0;
-        maxY = h - sprite.getHeight();
-
-        labelY = h - 5;
-    }
-
-    private void moreBunnies(int more) {
-        for (int i = 0; i < more; i++) {
-            bunnies.add(initBunny(new Bunny()));
-        }
-
-        bunnyLabel = String.valueOf(bunnies.size);
-    }
-
-    private void lessBunnies() {
-        if (bunnies.size <= 100) {
-            bunnies.clear();
-        } else {
-            bunnies.removeRange(bunnies.size-100, bunnies.size-1);
-        }
-
-        bunnyLabel = String.valueOf(bunnies.size);
-    }
-
-    private Bunny initBunny(Bunny bunny) {
-        bunny.x = minX + random.nextInt(MathUtils.floor(maxX - minX));
-        bunny.y = minY + random.nextInt(MathUtils.floor(maxY - minY));
-
-        do {
-            bunny.xv = 100 - random.nextInt(200);
-        } while (bunny.xv < 10 && bunny.xv > -10);
-
-        do {
-            bunny.yv = 100 - random.nextInt(200);
-        } while (bunny.yv < 10 && bunny.yv > -10);
-
-        bunny.r = random.nextInt(360);
-
-        do {
-            bunny.rv = 50 - random.nextInt(100);
-        } while (bunny.rv < 10 && bunny.rv > -10);
-
-        return bunny;
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+        if (Gdx.app.getType() != Application.ApplicationType.Desktop && Gdx.app.getType() != Application.ApplicationType.WebGL)
+            viewport.setUnitsPerPixel(1/Gdx.graphics.getDensity());
+        maxX = camera.viewportWidth - sprite.getWidth();
+        maxY = camera.viewportHeight - sprite.getHeight();
+        labelY = camera.viewportHeight - 5;
+        // String.format not available in GWT
+        sizeLabel = width + "x" + height + "(" + Math.round(camera.viewportWidth) + "x" + Math.round(camera.viewportHeight) + ") +" + Gdx.graphics.getDensity();
     }
 
     private void renderBunny(Bunny bunny, float dt) {
-        bunny.x += dt * bunny.xv;
-        if (bunny.x < minX) { bunny.x = minX; bunny.xv = -bunny.xv; }
-        if (bunny.x > maxX) { bunny.x = maxX; bunny.xv = -bunny.xv; }
+        bunny.x += dt * bunny.speedX;
+        if (bunny.x < minX) { bunny.x = minX; bunny.speedX = -bunny.speedX; }
+        if (bunny.x > maxX) { bunny.x = maxX; bunny.speedX = -bunny.speedX; }
 
-        bunny.y += dt * bunny.yv;
-        if (bunny.y < minY) { bunny.y = minY; bunny.yv = -bunny.yv; }
-        if (bunny.y > maxY) { bunny.y = maxY; bunny.yv = -bunny.yv; }
-
-        bunny.r = (bunny.r + dt * bunny.rv) % 360;
+        bunny.y += dt * bunny.speedY;
+        if (bunny.y < minY) { bunny.y = minY; bunny.speedY = -bunny.speedY; }
+        if (bunny.y > maxY) { bunny.y = maxY; bunny.speedY = -bunny.speedY; }
 
         sprite.setPosition(bunny.x,bunny.y);
-        sprite.setRotation(bunny.r);
-        sprite.setScale(0.1f + Math.abs(MathUtils.sinDeg(bunny.r)));
         sprite.draw(batch);
     }
 
@@ -169,12 +94,20 @@ public class BunnyMark extends ApplicationAdapter {
 
     @Override
 	public void render() {
+    	
+    	if (Gdx.input.isTouched()) {
+    		for (int i = 0; i < 10; i++) {
+    			bunnies.add(new Bunny());
+    		}
+    		bunnyLabel = String.valueOf(bunnies.size);
+    	}
+    	
         float dt = Gdx.graphics.getDeltaTime();
 
         fpsTime  += dt;
         fpsCount++;
         if (fpsTime > 1.0) {
-            fpsLabel = String.valueOf((((float)fpsCount) / fpsTime)).substring(0,5) + " FPS";
+            fpsLabel = String.valueOf((((float)fpsCount) / fpsTime)).substring(0,5) + " FPS ";
             fpsTime  = 0;
             fpsCount = 0;
         }
@@ -182,6 +115,8 @@ public class BunnyMark extends ApplicationAdapter {
         Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
         batch.begin();
 
         for (int i = 0, c = bunnies.size; i < c; i++) {
@@ -190,6 +125,7 @@ public class BunnyMark extends ApplicationAdapter {
 
         labelFont.draw(batch, bunnyLabel, labelX, labelY);
         labelFont.draw(batch, fpsLabel  , labelX, labelY - 15);
+        labelFont.draw(batch, sizeLabel, labelX, labelY - 30);
 
 		batch.end();
 	}
